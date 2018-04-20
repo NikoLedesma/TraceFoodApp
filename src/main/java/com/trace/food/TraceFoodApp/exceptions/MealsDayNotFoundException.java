4831565a38cb37e803a118f428af0007ecdeeb15
NicolasLed <n.e.ledesma@hotmail.com>
2018-04-20 00:50:36 -0300

    Exception class added

---
+++ src/main/java/com/trace/food/TraceFoodApp/exceptions/MealsDayNotFoundException.java
@@ -0,0 +1,7 @@
+package com.trace.food.TraceFoodApp.exceptions;
+
+public class MealsDayNotFoundException extends RuntimeException{
+    public MealsDayNotFoundException(String exception){
+        super(exception);
+    }
+}
